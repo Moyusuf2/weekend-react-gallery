8b6b32b47272da18c8bdeb028ac80f084cd287d6
Mohamed Yusuf <Mohamed-yusuf2@hotmail.com>
2022-10-23 17:18:17 -0500

    add css style

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,8 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 2, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
-    { id: 4, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/naruto.jpg', description: 'Naruto is a Japanese manga series written and illustrated by Masashi Kishimoto. It tells the story of Naruto Uzumaki, a young ninja who seeks recognition from his peers and dreams of becoming the Hokage, the leader of his village.', likes: 0 },
+    { id: 2, path: 'images/mockingspongebobbb.jpeg', description: 'Mocking SpongeBob meme.', likes: 0 },
+    { id: 3, path: 'images/attack_on_titan.jpg', description: 'Eren Yeager is determined to help save humanity when titans re-appear and being to feast on human flesh.', likes: 0 },
+    { id: 4, path: 'images/demon_Slayer.jpg', description: 'a young boy who becomes a demon slayer after his family is slaughtered and his younger sister Nezuko is turned into a demon.', likes: 0 }
 
 ];
 
